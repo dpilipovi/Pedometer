55c3da40905e90441d838b623ffe9e242833eeff
Korisnik <david.pilipovic9@gmail.com>
2020-06-08 16:27:47 +0200

    initial

--- app/src/main/java/android/tvz/hr/pedometer/MainActivity.kt
+++ app/src/main/java/android/tvz/hr/pedometer/MainActivity.kt
@@ -6,50 +6,72 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Toast
-import kotlinx.android.synthetic.main.main_activity.*
 
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
-
-    var running = false
-    var sensorManager:SensorManager? = null
-
+    private lateinit var textView: TextView
+    private var MagnitudePrevious = 0.0
+    private var stepCount = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-    }
+        textView = findViewById(R.id.steps_count)
+        val sensorManager =
+            getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sensor =
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val stepDetector: SensorEventListener = object : SensorEventListener {
+            override fun onSensorChanged(sensorEvent: SensorEvent) {
+                if (sensorEvent != null) {
+                    val x_acceleration = sensorEvent.values[0]
+                    val y_acceleration = sensorEvent.values[1]
+                    val z_acceleration = sensorEvent.values[2]
+                    val Magnitude =
+                        Math.sqrt(x_acceleration * x_acceleration + y_acceleration * y_acceleration + (z_acceleration * z_acceleration).toDouble())
+                    val MagnitudeDelta = Magnitude - MagnitudePrevious
+                    MagnitudePrevious = Magnitude
+                    if (MagnitudeDelta > 6) {
+                        stepCount++
+                    }
+                    textView.setText(stepCount.toString())
+                }
+            }
 
-    override fun onResume() {
-        super.onResume()
-        running = true
-        var stepsSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-
-        if (stepsSensor == null) {
-            Toast.makeText(this, "No Step Counter Sensor !", Toast.LENGTH_SHORT).show()
-        } else {
-            sensorManager?.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
+            override fun onAccuracyChanged(
+                sensor: Sensor,
+                i: Int
+            ) {
+            }
         }
+        sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onPause() {
         super.onPause()
-        running = false
-        sensorManager?.unregisterListener(this)
+        val sharedPreferences =
+            getPreferences(Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.clear()
+        editor.putInt("stepCount", stepCount)
+        editor.apply()
     }
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    override fun onStop() {
+        super.onStop()
+        val sharedPreferences =
+            getPreferences(Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.clear()
+        editor.putInt("stepCount", stepCount)
+        editor.apply()
     }
 
-    override fun onSensorChanged(event: SensorEvent) {
-        if (running) {
-            steps_count.setText("" + event.values[0])
-        }
-        else
-        {
-            steps_count.setText("ne radi")
-        }
+    override fun onResume() {
+        super.onResume()
+        val sharedPreferences =
+            getPreferences(Context.MODE_PRIVATE)
+        stepCount = sharedPreferences.getInt("stepCount", 0)
     }
 }
---
+++ app/src/main/java/android/tvz/hr/pedometer/StepActivity.kt
@@ -0,0 +1,109 @@
+package android.tvz.hr.pedometer
+
+import android.annotation.TargetApi
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class StepActivity : AppCompatActivity() {
+    var stepCount: TextView? = null
+    var detectCount: TextView? = null
+    var startServiceBtn: Button? = null
+    var stopServiceBtn: Button? = null
+    var countedStep: String? = null
+    var detectedStep: String? = null
+    var isServiceStopped = false
+
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        // ___ instantiate intent ___ \\
+        //  Instantiate the intent declared globally - which will be passed to startService and stopService.
+        intent = Intent(this, StepCountService::class.java)
+        init() // Call view initialisation method.
+    }
+
+    // Initialise views.
+    fun init() {
+        isServiceStopped = true // variable for managing service state - required to invoke "stopService" only once to avoid Exception.
+
+
+        // ________________ Service Management (Start & Stop Service). ________________ //
+        // ___ start Service & register broadcast receiver ___ \\
+        startServiceBtn =
+            findViewById<View>(R.id.start_button) as Button
+        startServiceBtn!!.setOnClickListener { // start Service.
+            startService(Intent(baseContext, StepCountService::class.java))
+            // register our BroadcastReceiver by passing in an IntentFilter. * identifying the message that is broadcasted by using static string "BROADCAST_ACTION".
+            registerReceiver(
+                broadcastReceiver,
+                IntentFilter("android.tvz.hr.StepCountService") // ?????????????????? NEMAM POJMA STA TREBA BIO JE "com.example.yusuf,mybroadcast" a to ne postoji
+            )
+            isServiceStopped = false
+        }
+
+        // ___ unregister receiver & stop service ___ \\
+        stopServiceBtn =
+            findViewById<View>(R.id.start_button) as Button
+        stopServiceBtn!!.setOnClickListener {
+            if (!isServiceStopped) {
+                // call unregisterReceiver - to stop listening for broadcasts.
+                unregisterReceiver(broadcastReceiver)
+                // stop Service.
+                stopService(Intent(baseContext, StepCountService::class.java))
+                isServiceStopped = true
+            }
+        }
+        // ___________________________________________________________________________ \\
+
+
+        stepCount = findViewById<View>(R.id.steps_count) as TextView
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    // --------------------------------------------------------------------------- \\
+    // ___ create Broadcast Receiver ___ \\
+    // create a BroadcastReceiver - to receive the message that is going to be broadcast from the Service
+    private val broadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(
+            context: Context,
+            intent: Intent
+        ) {
+            // call updateUI passing in our intent which is holding the data to display.
+            updateViews(intent)
+        }
+    }
+
+    // --------------------------------------------------------------------------- \\
+    // ___ retrieve data from intent & set data to textviews __ \\
+    private fun updateViews(intent: Intent) {
+        // retrieve data out of the intent.
+        countedStep = intent.getStringExtra("Counted_Step")
+        detectedStep = intent.getStringExtra("Detected_Step");
+        stepCount!!.text = '"'.toString() + countedStep.toString() + '"' + " Steps Counted"
+        detectCount!!.text = '"'.toString() + detectedStep.toString() + '"' + " Steps Counted"
+    }
+
+    companion object {
+        const val State_Count = "Counter"
+        const val State_Detect = "Detector"
+        private const val TAG = "SensorEvent"
+    }
+}
