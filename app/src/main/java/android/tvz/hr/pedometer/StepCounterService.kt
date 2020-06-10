9714483d1eee19cbc79e568537cec798b4b11381
Meapzer <meapzer@gmail.com>
2020-06-10 15:22:20 +0200

    fixed crash when switching fragments

--- app/src/main/java/android/tvz/hr/pedometer/StepCounterService.kt
+++ app/src/main/java/android/tvz/hr/pedometer/StepCounterService.kt
@@ -79,13 +79,9 @@ class StepCounterService : Service() {
 
         sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL)
 
-
         handler.removeCallbacks(sendUpdatesToUI)
         handler.postDelayed(sendUpdatesToUI, 1000)
 
-
-
-       // return super.onStartCommand(intent, flags, startId)
         return START_STICKY
     }
 
